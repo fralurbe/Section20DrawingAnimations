cb993c9338b80306e0691e20c7a9f5a9788ca411
Fran Lurbe <francisco.lurbe@gmail.com>
2022-09-26 13:58:07 +0200

    avances

--- timer/app.js
+++ timer/app.js
@@ -1,19 +1,17 @@
-class Timer {
-    constructor(durationInput, startButton, pauseButton){
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;   
-        
-        this.startButton.addEventListener('click', this.start);
-    }
-    start(){
-        console.log(this);
-    }
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
-timer.start();
+const timer = new Timer(durationInput, startButton, pauseButton,{
+    onStart() {
+        console.log('Timer started');
+    },
+    onTick() {
+        console.log('Timer onTick');
+    },
+    onComplete() {
+        console.log('Timer onComplete');
+    }
+});
+
+ 
